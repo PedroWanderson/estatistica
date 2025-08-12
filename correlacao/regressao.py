b3c21671d07b05dccbefda7ff0528ff458060276
EFG <efg@efg.local>
2025-08-11 21:19:25 -0300

    Intercepto e gráfixo de regressão

--- correlacao/regressao.py
+++ correlacao/regressao.py
@@ -11,3 +11,26 @@ print(dataframe.head())
 
 dataframe.plot.scatter(x = 'Exposure', y = 'PEFR')
 plt.show()
+
+# 3. Configuração e treinamento do modelo
+# Define a variáve preditora (independente), que é
+# a Exposure e a variável de resultado que é o PEFR
+predictors = ['Exposure']
+outcome = 'PEFR'
+# Instanciar o modelo
+model = LinearRegression()
+# Etapa de treinamento
+model.fit(dataframe[predictors], dataframe[outcome])
+
+#4. Exibição dos Coeficientes
+# intercepto
+print(f'Intercepto: {model.intercept_:.3f}')
+# Coeficiente angular
+print(f'Coeficiente Angular: {model.coef_[0]}')
+
+# 5. Geração do gráfico
+fig, (reg) = plt.subplots(1, 1, figsize=(4, 4))
+# Gráfico de regreção
+reg = sns.regplot(x = 'Exposure', y = 'PEFR', data= dataframe, ax= reg)
+plt.tight_layout()
+plt.show()
